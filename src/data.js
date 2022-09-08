980e02ad22135ca5a1eb4f6b08fb934ecba9efc1
Morteza <morteza.abolfathi73@gmail.com>
2022-09-08 09:11:00 +0430

    create 3 files and update 1 file backend  and data

---
+++ src/data.js
@@ -0,0 +1,235 @@
+// import bcrypt from "bcryptjs";
+
+export const products = [
+  {
+    id: 1,
+    name: "صندلی",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "ارسال رایگان" },
+      { support: "اورجینال" },
+    ],
+    price: 120,
+    offPrice: 120,
+    discount: 0,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p9i-600x600.jpg",
+  },
+  {
+    id: 2,
+    name: "صندلی",
+    description: [{ support: "گارانتی مادام العمر" }, { support: "اورجینال" }],
+    price: 110,
+    offPrice: 100,
+    discount: 8,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p4-600x600.jpg",
+  },
+  {
+    id: 3,
+    name: "صندلی",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 99,
+    offPrice: 89,
+    discount: 10,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p1i-600x600.jpg",
+  },
+  {
+    id: 4,
+    name: "صندلی",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 260,
+    offPrice: 220,
+    discount: 15,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p6-600x600.jpg",
+  },
+  {
+    id: 5,
+    name: "صندلی",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 150,
+    offPrice: 150,
+    discount: 0,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p2-600x600.jpg",
+  },
+  {
+    id: 6,
+    name: "صندلی",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p1-600x600.jpg",
+  },
+  {
+    id: 7,
+    name: "مبل",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image :"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p26i-600x600.jpg"
+  },
+  {
+    id: 8,
+    name: "مبل",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image :"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p24-600x600.jpg"
+  },
+  {
+    id: 9,
+    name: "مبل",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 34,
+    offPrice: 200,
+    discount: 13,
+    image: "https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p26-600x600.jpg"
+  },
+  {
+    id: 10,
+    name: "مبل",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 99,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p6-600x600.jpg"
+  },
+  {
+    id: 11,
+    name: "ساعت",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 35,
+    offPrice: 200,
+    discount: 13,
+    image :"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p13-600x600.jpg"
+  },
+  {
+    id: 12,
+    name: "ساعت",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 65,
+    offPrice: 200,
+    discount: 13,
+    image :"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p12-600x600.jpg"
+  },
+  {
+    id: 13,
+    name: "ساعت",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 60,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p11-600x600.jpg"
+  },
+  {
+    id: 14,
+    name: "ساعت",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 70,
+    offPrice: 200,
+    discount: 13,
+    image :"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p10i-600x600.jpg"
+  },
+  {
+    id: 15,
+    name: "لامپ",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 31,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p15-600x600.jpg"
+  },
+  {
+    id: 16,
+    name: "لامپ",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 81,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p17-600x600.jpg"
+  },
+  {
+    id: 17,
+    name: "لامپ",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 34,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p16i-600x600.jpg"
+  },
+  {
+    id: 18,
+    name: "لامپ",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 120,
+    offPrice: 200,
+    discount: 13,
+    image:"https://xtratheme.ir/elementor/furniture/wp-content/uploads/sites/14/2019/09/p17-600x600.jpg"
+  },
+];
+
